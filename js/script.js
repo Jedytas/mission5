0845f99e8f8caa949664eba217b9650f165970bd
Jedytas <110136668+Jedytas@users.noreply.github.com>
2023-10-19 17:01:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const productPrice = parseInt(productSelect.value);
         const quantity = parseInt(quantityInput.value);
 
-        if (Number.isNaN(productPrice) || Number.isNaN(quantity)) {
+        // Проверка корректности ввода данных
+        const validInput = /^-?\d+$/.test(quantityInput.value);
+
+        if (!validInput) {
+            resultParagraph.textContent = "Введите корректное количество.";
+        } else if (
+            Number.isNaN(productPrice) ||
+            Number.isNaN(quantity) ||
+            quantity < 0
+        ) {
             resultParagraph.textContent = "Введите корректные данные.";
         } else {
             const totalPrice = productPrice * quantity;
